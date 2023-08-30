4b06abbfb939cc36505c4a83ea0285f756abeed7
Ritesh R <riteshravichandran.b@outlook.com>
2023-08-30 13:38:35 +0530

    User Registration

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     #path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
     path("company/<int:pk>", views.company_record, name="company"),
     path("employee/<int:pk>", views.employee_record, name="employee"),
     path("delete_employee/<int:pk>", views.delete_employee_record, name="delete_employee"),
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 from .models import Company, Employee
 
 
@@ -19,10 +20,29 @@ def home(request):
             messages.success(request, "Login Successful!")
             return redirect('home')
         else:
-            messages.success(request, "Incorrect Credentials")
+            messages.success(request, "Incorrect Credentials...")
             return redirect('home')
     else:
         return render(request, "home.html", {'companies':companies, 'employees':employees})
+
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
 
 
 def logout_user(request):
